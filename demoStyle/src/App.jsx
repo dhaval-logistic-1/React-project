3b7704ab27a2e39b22e9d4fe41e15feaef628037
Dhaval <dhavalparmar@logisticinfotech.co.in>
2026-03-27 14:26:16 +0530

    Add Project

---
+++ demoStyle/src/App.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./styles/main.scss";
+
+
+const App = () => {
+  return (
+
+
+    <div className="container ">
+
+      <h1 className="text-primary ">Hello Bootstrap</h1>
+
+      <button type="button" className="btn btn-primary ">Primary</button>
+      <button type="button" className="btn btn-secondary">Secondary</button>
+      <button type="button" className="btn btn-success">Success</button>
+      <button type="button" className="btn btn-danger">Danger</button>
+      <button type="button" className="btn btn-warning">Warning</button>
+      <button type="button" className="btn btn-info">Info</button>
+      <button type="button" className="btn btn-light"> Light</button>
+      <button type="button" className="btn btn-dark">Dark</button>
+      <button type="button" className="btn btn-link">Link</button>
+
+
+
+      <h1 className="text-primary ">Hello Bootstrap</h1>
+      <h1 className="text-secondary">Hello Bootstrap</h1>
+      <h1 className="text-success">Hello Bootstrap</h1>
+      <h1 className="text-danger">Hello Bootstrap</h1>
+      <h1 className="text-warning">Hello Bootstrap</h1>
+
+
+      {/* <nav className="navbar navbar-expand-lg text-warning bg-light">
+        <a className="navbar-brand" href="#">
+          Bootstrap
+        </a>
+        
+        <div className="collapse navbar-collapse " id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+          </ul>
+        </div>  
+
+      </nav> */}
+
+    </div>
+
+    
+
+
+  );
+};
+
+export default App;
---
+++ demoStyle/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+        silenceDeprecations: [
+          "import",
+        ],
+      },
+    },
+  },
+
+  sassOptions: {
+    quietDeps: true,
+  },
+});
